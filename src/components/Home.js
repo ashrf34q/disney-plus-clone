2d6c86983c21c012877aea7dfecd208b9bcad7a1
Ashraf Makki <makkiashraf08@gmail.com>
2022-12-31 21:39:50 -0500

    Added Carousel. Still need to make it responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import ImgCarousel from "./ImgCarousel";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <ImgCarousel />
+    </Container>
+  );
 }
 
 // export the component to be imported in another component
 export default Home;
+
+const Container = styled.main`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+
+  &:before {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    /* behind the content at all times */
+    z-index: -1;
+  }
+`;
---
+++ src/components/ImgCarousel.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styled from "styled-components";
+
+function ImgCarousel() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <img src="/images/slider-badging.jpg" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-badag.jpg" />
+      </Wrap>
+    </Carousel>
+  );
+}
+
+export default ImgCarousel;
+
+const Carousel = styled(Slider)`
+  margin-top: 24px;
+`;
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
