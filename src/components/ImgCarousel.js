617add9498a25397e2f1fac4375b67b3ecee3ed7
Ashraf Makki <makkiashraf08@gmail.com>
2023-01-01 15:50:07 -0500

    Finished Carousel element

--- src/components/ImgCarousel.js
+++ src/components/ImgCarousel.js
@@ -22,6 +22,12 @@ function ImgCarousel() {
       <Wrap>
         <img src="/images/slider-badag.jpg" />
       </Wrap>
+      <Wrap>
+        <img src="/images/slider-scale.jpg" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-scales.jpg" />
+      </Wrap>
     </Carousel>
   );
 }
@@ -30,9 +36,40 @@ export default ImgCarousel;
 
 const Carousel = styled(Slider)`
   margin-top: 24px;
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  li.slick-active button::before {
+    color: #fff;
+  }
+
+  button {
+    z-index: 1;
+  }
+
+  .slick-list {
+    overflow: visible;
+  }
 `;
 
 const Wrap = styled.div`
-  width: 100%;
-  height: 100%;
+  img {
+    width: 100%;
+    border-radius: 5px;
+    border: 4px solid transparent;
+    box-shadow: rgb(0 0 0 / 69%) 0 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition-duration: 300ms;
+
+    &:hover {
+      border: 4px solid #f8f9fa;
+    }
+  }
+
+  cursor: pointer;
 `;
