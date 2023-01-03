c78a55d0c246cdf48d88200249fc6daea761b487
Ashraf Makki <makkiashraf08@gmail.com>
2023-01-03 14:48:58 -0500

    Added React router DOM

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,7 +7,16 @@ function Movies() {
       <h4>Recommended for you</h4>
       <Content>
         <Wrap>
-          <img src="" />
+          <img src="/images/simpsons.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="/images/simpsons.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="/images/simpsons.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="/images/simpsons.jpg" />
         </Wrap>
       </Content>
     </Container>
@@ -18,12 +27,35 @@ export default Movies;
 
 const Container = styled.div`
   margin-top: 32px;
+
+  h4 {
+    margin-bottom: 24px;
+  }
 `;
 
 const Content = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(4, minmax(0, 1fr));
   grid-gap: 24px;
 `;
 
-const Wrap = styled.div``;
+const Wrap = styled.div`
+  border-radius: 10px;
+  overflow: hidden;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0 0 0 /69%) 0 26px 30px 10px,
+    rgb(0 0 0 / 73%) 0 16px 10px -10px;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    border-color: rgba(249, 250, 249, 0.8);
+  }
+`;
